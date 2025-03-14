805ccee0f3ab4079629da883bb62a8cabea25060
Everts Rozenbaums <evertrozenbaum@gmail.com>
2025-03-14 22:23:00 +0200

    fix: yarn make not working #5 & yarn upgrade

--- forge.config.js
+++ forge.config.js
@@ -9,12 +9,11 @@ module.exports = {
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      platforms: ["win32"],
       config: {},
     },
     {
       name: "@electron-forge/maker-zip",
-      platforms: ["darwin"],
+      platforms: ["darwin", "win32"],
     },
     {
       name: "@electron-forge/maker-deb",
